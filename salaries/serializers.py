e3206ca35b39967e7521301cac2aef9d04388ee0
username <username@example.com>
2025-04-27 16:11:13 +0800

    前后端分离的全部查询完成，差条件查询和排序分页功能，还有一些权限的问题。

--- salaries/serializers.py
+++ salaries/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from.models import Salary
+from .models import Salary
 
 class SalarySerializer(serializers.ModelSerializer):
+    emp_name = serializers.ReadOnlyField(source='emp_user.emp_name')
+    create_date = serializers.DateTimeField(source='created_at', format='%Y-%m-%d')  # 新增日期格式化字段
+    department_name = serializers.ReadOnlyField(source='emp_user.department.dept_name')  # 修正外键路径
+    
     class Meta:
         model = Salary
         fields = '__all__'
-        read_only_fields = ['year','month','final_salary','leave_days','deduction_days']
+        read_only_fields = [
+            'year', 'month', 
+            'final_salary', 'leave_days',
+            'deduction_days', 'created_at'  # 新增原始日期字段保护
+        ]
